1a242f4012b9c95e31e83ecc46793dcd5cf9aad0
khantanil <khantradhe45@gmail.com>
2023-01-17 20:06:21 +0530

    hello

--- Project/calculator/index.js
+++ Project/calculator/index.js
@@ -3,16 +3,13 @@ let output = document.getElementById('output');
 function display(val) {
     document.getElementById('output').value += val
     return val
-
 }
 
 function solve() {
-
     let x = document.getElementById('output').value
     let y = eval(x);
     document.getElementById('output').value = y
     return y
-
 }
 
 function clearScreen() {
