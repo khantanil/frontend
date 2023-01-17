5ecba6a5181107f6cb14b54ade9c1c79ab326abf
khantanil <khantradhe45@gmail.com>
2023-01-17 20:03:40 +0530

    calc

---
+++ Project/calculator/index.js
@@ -0,0 +1,20 @@
+let output = document.getElementById('output');
+
+function display(val) {
+    document.getElementById('output').value += val
+    return val
+
+}
+
+function solve() {
+
+    let x = document.getElementById('output').value
+    let y = eval(x);
+    document.getElementById('output').value = y
+    return y
+
+}
+
+function clearScreen() {
+    document.getElementById('output').value = ''
+}
